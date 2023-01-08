7a351fa25ef977fb6222705cd5fd2b64170a32e1
hata48915b <hata48915b@post.nifty.jp>
2023-01-09 08:18:29 +0900

    update logo

--- image/.makelogo.sh
+++ image/.makelogo.sh
@@ -7,7 +7,23 @@ convert -stroke white -strokewidth 10 \
 	-stroke white -strokewidth 5 \
 	-pointsize 96 -fill white -annotate +128-30 '→' \
 	-pointsize 96 -fill white -annotate +128+10 '←' \
-	md8docx.png
+	-depth 2 \
+	md8docx.xpm
 
-convert -resize 128x128! md8docx.png md4docx.png
+convert -stroke white -strokewidth 10 \
+	\( -size 256x256 xc:black -gravity center -pointsize 128 -fill white -annotate +0+0 'M' \) \
+	\( -size 256x256 xc:blue  -gravity center -pointsize 128 -fill white -annotate +0+0 'W' \) \
+	+append \
+	-stroke white -strokewidth 5 \
+	-pointsize 96 -fill white -annotate +128-30 '→' \
+	-pointsize 96 -fill white -annotate +128+10 '←' \
+	-resize 256x256! \
+	-depth 2 \
+	md4docx.xpm
+
+convert -depth 2 md8docx.xpm md8docx.png
+convert -depth 2 md4docx.xpm md4docx.png
+
+rm md8docx.xpm
+rm md4docx.xpm
 
