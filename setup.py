ec1af047363b55a4eb4917496986ec0aaf857fd4
hata48915b <hata48915b@post.nifty.jp>
2024-07-06 09:45:00 +0900

    upgrade to v07.08

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='makdo',
-    version='07.07',
+    version='07.08',
     description='日本の公用文書（司法文書、行政文書）をMarkdown形式とMicrosoft Word形式との間で変換します',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
