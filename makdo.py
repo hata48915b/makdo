5661ea6b2d7bdc174abe78f652f50a8793390373
hata48915b <hata48915b@post.nifty.jp>
2024-09-28 13:02:00 +0900

    add makdo.py

---
+++ makdo.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+# Name:         makdo.py
+# Version:      v07 Furuichibashi
+# Time-stamp:   <2024.09.28-13:00:10-JST>
+
+# makdo.py
+# Copyright (C) 2022-2024  Seiichiro HATA
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# 2022.07.21 v01 Hiroshima
+# 2022.08.24 v02 Shin-Hakushima
+# 2022.12.25 v03 Yokogawa
+# 2023.01.07 v04 Mitaki
+# 2023.03.16 v05 Aki-Nagatsuka
+# 2023.06.07 v06 Shimo-Gion
+# 2024.04.02 v07 Furuichibashi
+
+
+import argparse
+
+
+__version__ = 'v07 Furuichibashi'
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='MS WordファイルやMarkdownファイルに作成します',
+        add_help=False)
+    parser.add_argument(
+        '-h', '--help',
+        action='help',
+        help='ヘルプメッセージを表示します')
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version=('%(prog)s ' + __version__),
+        help='バージョン番号を表示します')
+    parser.add_argument(
+        'input_file',
+        default='',
+        nargs='?',
+        help='MS Wordファイル又はMarkdownファイル')
+    return parser.parse_args()
+args = get_arguments()
+
+
+from makdo import Makdo
+if args.input_file != '':
+    Makdo.args_input_file = args.input_file
+Makdo()
