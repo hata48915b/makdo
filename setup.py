4b7a40eb13a8159e1c8ffc7b9ecb71ff06e60619
hata48915b <hata48915b@post.nifty.jp>
2024-09-30 07:53:44 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import sys
 from setuptools import setup
+
+VERSION = '07.09'
+
+INSTALL_REQUIRES = ['python-docx', 'chardet', 'tkinterdnd2', 'openpyxl', 'openai', 'Levenshtein']
+if sys.platform == 'win32':
+    INSTALL_REQUIRES.append('pywin32')
 
 setup(
     name='makdo',
-    version='07.09',
+    version=VERSION,
     description='日本の公用文書（司法文書、行政文書）をMarkdown形式とMicrosoft Word形式との間で変換します',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
@@ -10,6 +17,6 @@ setup(
     author_email='hata48915b@post.nifty.jp',
     url='https://github.com/hata48915b/makdo/',
     license='GPLv3+',
-    install_requires=['python-docx', 'chardet', 'tkinterdnd2', 'pywin32', 'openpyxl', 'openai', 'Levenshtein'],
+    install_requires=INSTALL_REQUIRES,
     packages=['makdo'],
 )
