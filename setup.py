8009156330e078fe1eb9bb9f611440438db31fb1
hata48915b <hata48915b@post.nifty.jp>
2024-12-25 08:44:54 +0900

    upgrade to v07.18

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 from setuptools import setup
 
-VERSION = '07.18'
+VERSION = '07.19'
 
 INSTALL_REQUIRES = ['python-docx', 'chardet', 'Levenshtein', 'openpyxl']
 if sys.platform == 'win32':
