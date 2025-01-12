ef57295a4d2be807aba44393fd511e5cdc524a28
hata48915b <hata48915b@post.nifty.jp>
2025-01-12 09:35:09 +0900

    update images

--- image/.make_images.sh
+++ image/.make_images.sh
@@ -15,16 +15,15 @@ cat x0.xpm \
 
 # MAKE THE APPLI IMAGE
 
-# convert -quality 100 -resize 2048x2048! -depth 8 -colors 512 xB.xpm makdoB.png
-# convert -quality 100 -resize 1024x1024! -depth 8 -colors 512 xB.xpm makdoA.png
-# convert -quality 100 -resize  512x512!  -depth 8 -colors 512 xB.xpm makdo9.png
-convert -quality 100 -resize  256x256!  -depth 8 -colors 512 xB.xpm makdo8.png
-# convert -quality 100 -resize  128x128!  -depth 8 -colors 512 xB.xpm makdo7.png
-# convert -quality 100 -resize   64x64!   -depth 8 -colors 512 xB.xpm makdo6.png
-# convert -quality 100 -resize   32x32!   -depth 8 -colors 512 xB.xpm makdo5.png
-# convert -quality 100 -resize   16x16!   -depth 8 -colors 512 xB.xpm makdo4.png
-convert -quality 100 -resize  512x256!  -depth 8 -colors 512 xB.xpm makdoL.png
-chmod 444 makdo8.png makdoL.png
+# convert -quality 100 -resize 2048x2048! -depth 8 -colors 512 xB.xpm makdoB.png; chmod 444 makdoB.png
+# convert -quality 100 -resize 1024x1024! -depth 8 -colors 512 xB.xpm makdoA.png; chmod 444 makdoA.png
+# convert -quality 100 -resize  512x512!  -depth 8 -colors 512 xB.xpm makdo9.png; chmod 444 makdo9.png
+convert -quality 100 -resize  256x256!  -depth 8 -colors 512 xB.xpm makdo8.png; chmod 444 makdo8.png
+# convert -quality 100 -resize  128x128!  -depth 8 -colors 512 xB.xpm makdo7.png; chmod 444 makdo7.png
+# convert -quality 100 -resize   64x64!   -depth 8 -colors 512 xB.xpm makdo6.png; chmod 444 makdo6.png
+# convert -quality 100 -resize   32x32!   -depth 8 -colors 512 xB.xpm makdo5.png; chmod 444 makdo5.png
+# convert -quality 100 -resize   16x16!   -depth 8 -colors 512 xB.xpm makdo4.png; chmod 444 makdo4.png
+convert -quality 100 -resize  512x256!  -depth 8 -colors 512 xB.xpm makdoL.png; chmod 444 makdoL.png
 
 # MAKE THE SPLASH IMAGE
 
@@ -34,11 +33,11 @@ cat x0.xpm \
 					 > xB.xpm
 
 convert -quality 100 \
-	'(' -size 512x320 xc:#6F6F6F ')' \
-	'(' -resize  512x256! -depth 8 xB.xpm ')' \
-	-geometry +0+32 -composite \
+	'(' -size 512x320 xc:#7F7F7F ')' \
+	'(' -resize  256x256! -depth 8 xB.xpm ')' \
+	-geometry +128+32 -composite \
 	-font IPAゴシック -pointsize 22 -fill '#FFFF00' -stroke '#FFFF00' -strokewidth 0 -annotate +12+25   'MAKDO is starting.' \
-	-font IPAゴシック -pointsize 22 -fill '#FFFF00' -stroke '#FFFF00' -strokewidth 0 -annotate +195+313 'Developed by Seiichiro HATA.' \
+	-font IPAゴシック -pointsize 22 -fill '#FFFF00' -stroke '#FFFF00' -strokewidth 0 -annotate +220+313 'Powered by Seiichiro HATA.' \
 	makdo_splash.png
 chmod 444 makdo_splash.png
 
@@ -49,15 +48,15 @@ cat x0.xpm \
     | sed 's;#202060609F9F";None";g' | sed 's;#20609F";None";g' \
 					 > xB.xpm
 
-#convert -quality 100 -resize 2048x2048! -depth 8 -colors 5 xB.xpm $TMPDIR/icoB.png
+#convert -quality 100 -resize 2048x2048! -depth 8 -colors 512 xB.xpm $TMPDIR/icoB.png
 convert -quality 100 -resize 1024x1024! -depth 8 -colors 512 xB.xpm $TMPDIR/icoA.png
 convert -quality 100 -resize  512x512!  -depth 8 -colors 512 xB.xpm $TMPDIR/ico9.png
 convert -quality 100 -resize  256x256!  -depth 8 -colors 512 xB.xpm $TMPDIR/ico8.png
 convert -quality 100 -resize  128x128!  -depth 8 -colors 512 xB.xpm $TMPDIR/ico7.png
-#convert -quality 100 -resize   64x64!   -depth 8 -colors 5 xB.xpm $TMPDIR/ico6.png
+#convert -quality 100 -resize   64x64!   -depth 8 -colors 512 xB.xpm $TMPDIR/ico6.png
 convert -quality 100 -resize   32x32!   -depth 8 -colors 512 xB.xpm $TMPDIR/ico5.png
 convert -quality 100 -resize   16x16!   -depth 8 -colors 512 xB.xpm $TMPDIR/ico4.png
-#convert -quality 100 -resize  512x256!  -depth 8 -colors 5 xB.xpm $TMPDIR/icoL.png
+#convert -quality 100 -resize  512x256!  -depth 8 -colors 512 xB.xpm $TMPDIR/icoL.png
 
 # WINDOWS
 convert $TMPDIR/icoA.png -define icon:auto-resize=256,128,64,32,16 makdo_win.ico
@@ -72,8 +71,3 @@ rm x0.log x0.mp x0.ps x0.xpm xB.xpm
 
 rm $TMPDIR/ico?.png
 rmdir $TMPDIR
-
-# rm -f makdo8.png makdoL.png
-rm -f makdo_splash.png
-rm -f makdo_win.ico
-rm -f makdo_mac.icns
